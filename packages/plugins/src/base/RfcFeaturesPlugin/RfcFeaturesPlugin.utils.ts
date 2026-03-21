a7ebf5a8a5bc67ea95ab6a0b265c77795246027f
Silviu Glavan <silviu@gigs.ro>
2026-03-21 08:46:56 +0200

    chore(plugins): isSemanticNullable typedoc

--- packages/plugins/src/base/RfcFeaturesPlugin/RfcFeaturesPlugin.utils.ts
+++ packages/plugins/src/base/RfcFeaturesPlugin/RfcFeaturesPlugin.utils.ts
@@ -4,6 +4,14 @@ export const RfcDirective = {
   SEMANTIC_NON_NULL: "semanticNonNull",
 } as const;
 
+/**
+ * Determines if a field is semantically nullable based on its type and/or the presence of the `@semanticNonNull` directive.
+ *
+ * @param field Fiel node to check
+ * @param level Semantic nullability level
+ * @returns `false` if the field is non-nullable either by being a NonNullType or by having the `@semanticNonNull` directive at the specified level, `true` otherwise.
+ */
+
 export const isSemanticNullable = (field: FieldNode, level = 0) => {
   if (field.type instanceof NonNullTypeNode) {
     return false;
