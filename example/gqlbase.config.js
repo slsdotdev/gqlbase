eb60a5a21d9ab41844446bce890693ef6fd05abf
Silviu Glavan <silviu@gigs.ro>
2026-03-22 07:04:46 +0200

    fix(plugins): update relay exports

--- example/gqlbase.config.js
+++ example/gqlbase.config.js
@@ -1,8 +1,9 @@
 import { defineConfig } from "@gqlbase/cli/config";
 import { basePreset } from "@gqlbase/plugins/base";
+import { relayPreset } from "@gqlbase/plugins/relay";
 
 export default defineConfig({
   source: "src/schema/**/*.graphql",
   output: "generated",
-  plugins: [basePreset()],
+  plugins: [basePreset(), relayPreset()],
 });
